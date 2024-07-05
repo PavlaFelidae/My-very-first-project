a03bad4c31c20f0555f566834570dba3ebef7209
PavlaFelidae <alena1pakhomova@gmail.com>
2024-07-05 19:05:59 +0200

    Add files via upload

---
+++ Itproger/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Comment
+        System.out.println("НАПАС\n\tЛАВАНДОС\nПАЧКА ПАПИРОС");
+        /*asd
+        ads
+        asd
+         */
+    }
+}
